98d0a9c5e5ab224556ef65d0860f1d493a43363b
Benjamin Puhani <bpuhani@usfca.edu>
2025-04-05 15:41:27 -0700

    ADD: get notes api call

--- apps/backend/src/controllers/notes.controller.ts
+++ apps/backend/src/controllers/notes.controller.ts
@@ -1,5 +1,7 @@
-import type {Request, Response} from "express";
+import type {Response} from "express";
 import StatusCodes from "../types/response-codes";
+import type {UserRequest} from "../types";
+import UserModel from "../models/user.model";
 
 class NotesController {
 
@@ -7,15 +9,44 @@ class NotesController {
     /**
      * Save notes
      */
-    read(req: Request, res: Response) {
-        console.log("read notes");
-        res.status(StatusCodes.SUCCESS.code).json();
+    read(req: UserRequest, res: Response) {
+        const videoId = req.params.videoID as string;
+        UserModel.findOne({email: req.user?.email}).then(
+            (user) => {
+                if (!user) {
+                    res.status(StatusCodes.NOT_FOUND.code).json({
+                        message: "User not found",
+                    });
+                    return;
+                }
+                const userVideo = user.userVideos?.find((userVideo) => userVideo.videoId === videoId)
+
+                if (!userVideo) {
+                    res.status(StatusCodes.NOT_FOUND.code).json({
+                        message: "Video not found",
+                    });
+                    return;
+                }
+                const notes = userVideo.notes;
+
+                // success send the notes
+                res.status(StatusCodes.SUCCESS.code).json({
+                    notes: notes,
+                });
+            },
+        ).catch((error) => {
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR.code).json({
+                message: "Internal server error",
+            });
+        }
+        )
     }
 
     /**
      * Save notes
      */
-    save(req: Request, res: Response) {
+    save(req: UserRequest, res: Response) {
         console.log("save notes");
 
         res.status(StatusCodes.SUCCESS.code).json();
@@ -24,7 +55,7 @@ class NotesController {
     /**
      * Update notes
      */
-    patch(req: Request, res: Response) {
+    patch(req: UserRequest, res: Response) {
         console.log("update notes");
     }
 }
