45065247b717516a2b7f56d436f79bf1146a1ac0
Benjamin Puhani <bpuhani@usfca.edu>
2025-04-05 16:15:37 -0700

    FIX: endpoint of notes.ts /

--- apps/backend/src/routes/notes.ts
+++ apps/backend/src/routes/notes.ts
@@ -2,9 +2,8 @@ import { Router } from "express";
 import NotesController from "../controllers/notes.controller";
 const router = Router();
 
-router.get("/get", NotesController.read);
-router.post("/create", NotesController.save);
+router.get("/", NotesController.read);
+router.post("/", NotesController.save);
 router.patch("/update/:id", NotesController.patch);
-
 
 export default router;
