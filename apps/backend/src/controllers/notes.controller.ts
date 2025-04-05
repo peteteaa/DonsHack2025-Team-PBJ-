ed6bc3cfe51761c6fba23a129e85537705d5b897
Benjamin Puhani <bpuhani@usfca.edu>
2025-04-05 16:57:58 -0700

    ADD: function to create note

--- apps/backend/src/controllers/notes.controller.ts
+++ apps/backend/src/controllers/notes.controller.ts
@@ -1,36 +1,55 @@
 import type { Response } from "express";
-import UserModel from "../models/user.model";
 import type { UserRequest } from "../types";
 import StatusCodes from "../types/response-codes";
+import {validateUserAndVideo} from "../utils/validate_video_and_user";
 
 class NotesController {
+
 	/**
-	 * Save notes
+	 * Get notes of a specific User and Video
+	 * @param {UserRequest} req
+	 * @param {Response} res
 	 */
 	read(req: UserRequest, res: Response) {
 		const videoId = req.params.videoID as string;
-		UserModel.findOne({ email: req.user?.email })
-			.then((user) => {
-				if (!user) {
-					res.status(StatusCodes.NOT_FOUND.code).json({
-						message: "User not found",
-					});
-					return;
-				}
-				const userVideo = user.userVideos?.find(
-					(video) => video.videoId.toString() === videoId,
-				);
 
-				if (!userVideo) {
-					res.status(StatusCodes.NOT_FOUND.code).json({
-						message: "Video not found",
-					});
-					return;
-				}
-				const notes = userVideo.notes;
+		validateUserAndVideo(req, res, videoId).then((result) => {
+			if (!result) return;
 
-				// success send the notes
-				res.status(StatusCodes.SUCCESS.code).json(notes);
+			const { userVideo } = result;
+			const notes = userVideo.notes;
+
+			res.status(StatusCodes.SUCCESS.code).json(notes);
+		});
+	}
+
+	/**
+	 * Create note
+	 */
+	create(req: UserRequest, res: Response) {
+		const videoId = req.params.videoID as string;
+		const { note } = req.body;
+
+		validateUserAndVideo(req, res, videoId).then((result) => {
+			if (!result) {
+				return; // Early return if validation fails
+			}
+
+			const {user, userVideo} = result
+			userVideo.notes.push(note);
+
+				user.save()
+					.then(() => {
+						res.status(StatusCodes.SUCCESS.code).json({
+							message: "Note saved successfully",
+						});
+					})
+					.catch((error) => {
+						console.error(error);
+						res.status(StatusCodes.INTERNAL_SERVER_ERROR.code).json({
+							message: "Internal server error",
+						});
+					});
 			})
 			.catch((error) => {
 				console.error(error);
@@ -41,15 +60,6 @@ class NotesController {
 	}
 
 	/**
-	 * Save notes
-	 */
-	save(_req: UserRequest, res: Response) {
-		console.log("save notes");
-
-		res.status(StatusCodes.SUCCESS.code).json();
-	}
-
-	/**
 	 * Update notes
 	 */
 	patch(_req: UserRequest, res: Response) {
@@ -57,5 +67,7 @@ class NotesController {
 		res.status(StatusCodes.SUCCESS.code).json();
 	}
 }
+
+
 
 export default new NotesController();
